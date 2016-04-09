8f269ae9d72145acfd9cf93a3cdff1d20748a51f
sukeshni <sukeshni.kantrod@gmail.com>
2016-04-10 01:33:43 +0900

    small fixes, add post request for /:id/flowers

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,9 @@ router.use(require('../middlewares/auth'));
 // GET /users
 router.get('/', function (req, res, next) {
   // res.json(["users"]);
-  res.json(
+  res.status(200).json(
     {
-      "code": 200,
-      "user":[
+      "users":[
         {"id":1,"name":"owner1","image_url":null},
         {"id":2,"name":"owner2","image_url":null},
         {"id":3,"name":"owner3","image_url":null},
@@ -22,9 +21,8 @@ router.get('/', function (req, res, next) {
 
 // GET /users/:id
 router.get('/:id', function (req, res, next) {
-  res.json(
+  res.status(200).json(
     {
-      "code": 200,
       id: req.params.id, name: "John Doe", "image_url":null
     });
   return next();
@@ -32,15 +30,14 @@ router.get('/:id', function (req, res, next) {
 
 
 router.get('/:id/flowers', function (req, res, next) {
-  res.json(
+  res.status(200).json(
     {
-      "code": 200,
       "flowers":[
         {"name": "flower1", "count": 3},
         {"name": "flower2", "count": 4},
         {"name": "flower3", "count": 5},
       ],
-      "sender":[
+      "senders":[
         {"id": 1, "name": "user1", "image_url": null,"flower_name": "flower1", "message": "message1"},
         {"id": 2, "name": "user2", "image_url": null,"flower_name": "flower2", "message": "message2"},
         {"id": 3, "name": "user3", "image_url": null,"flower_name": "flower3", "message": "message3"},
@@ -49,5 +46,11 @@ router.get('/:id/flowers', function (req, res, next) {
     });
   return next();
 });
+
+router.post('/:id/flowers', function (req, res, next) {
+  res.status(201);
+  return next();
+});
+
 
 module.exports = router;
