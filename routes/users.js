7e4c343a7ae802c0901bfdd45df8ea20508de69f
sukeshni <sukeshni.kantrod@gmail.com>
2016-04-10 13:51:04 +0900

    Use db for /users

--- routes/users.js
+++ routes/users.js
@@ -8,105 +8,110 @@ router.use(require('../middlewares/auth'));
 
 // GET /users
 router.get('/', function (req, res, next) {
-  var users = [];
-  knex('users')
-  .then(function (rows) {
-    if(!rows[0]) {
-      res.status(404).send({
-        "message": "User Not Found"
-      });       
-    }
-    for ( var i=0; i<rows.length; i++ ) {
-      users[i] = {
-        "id": rows[i].id,
-        "name": rows[i].name,
-        "image_url": rows[i].image_url
+  let 
+    offset = req.body.offset || 0,
+    limit = req.body.limit || 20;
+  knex
+    .select('id', 'name', 'image_url')
+    .from('users')
+    .offset(offset).limit(limit)
+    .then(function (rows) {
+      if(!rows[0]) {
+        res.status(404).send({
+          "message": "User Not Found"
+        });       
       }
-    }
-    res.status(200).json({
-      "users": users
+      res.status(200).json(rows);
+    }).catch(function(err) {
+      res.status(500).json(err);
     });
-  }).catch(function(err) {
-    res.status(400).send({
-      "message": "Bad Request"
-    });
-  });
 });
 
 // GET /users/:id
 router.get('/:id', function (req, res, next) {
 
-  knex('users').where('id', req.params.id)
+  knex('users')
+  .join('user_flowers', 'users.id', '=', 'user_flowers.reciever_user_id')
+  .join('flowers', 'user_flowers.flower_id', '=', 'flowers.id')
+  .select('users.id', 'users.name', 'users.image_url', 'flowers.name as flower').count('flowers.id as count')
+  .groupBy('users.id', 'flowers.name')
+  .where({'users.id': req.params.id})
     .then(function (row) {
       if(!row[0]) {
-        res.status(404).send({
-          "message": "User Not Found"
-        });       
+        res.status(404).json(new Error("User Not Found"));
+        return next();
       }
-      res.status(200).send({
-        "id": row[0].id,
-        "name": row[0].name,
-        "image_url": row[0].image_url
+      let user = {
+        id: row[0].id,
+        name: row[0].name,
+        image_url: row[0].image_url,
+      }
+      let flowers = row.map(function(elem) {
+        return {
+          name: elem.flower,
+          count: elem.count
+        }
       });
+      res.status(200).json({
+        user: user,
+        flowers: flowers,
+      });
+      return next();
     }).catch(function(err) {
-      res.status(400);
+      res.status(400).json(err);
+      return next();
     });
 });
 
+router.get('/:id/flowers', function(req, res, next) {
+  let
+    begin_id = req.body.begin_id || 0,
+    limit    = req.body.limit || 20;
 
-router.get('/:id/flowers', function (req, res, next) {
-  res.status(200).json(
-    {
-      "flowers":[
-        {"name": "flower1", "count": 3},
-        {"name": "flower2", "count": 4},
-        {"name": "flower3", "count": 5},
-      ],
-      "send": [
-      {
-        "send_id": 2,
-        "sender": 
-          {"id": 2, "name": "user2", "image_url": null},
-          "flower_name": "flower2", 
-          "message": "message2"
-      },
-      {
-        "send_id": 3,
-        "sender": 
-          {"id": 3, "name": "user3", "image_url": null},
-          "flower_name": "flower3", 
-          "message": "message3"
-      },
-      {
-        "send_id": 4,
-        "sender": 
-          {"id": 4, "name": "user4", "image_url": null},
-          "flower_name": "flower4", 
-          "message": "message4"
-      }]
+  knex('user_flowers as uf')
+    .join('flowers', 'uf.flower_id', '=', 'flowers.id')
+    .join('users as sender', 'uf.sender_user_id', '=', 'sender.id')
+    .select('uf.id', 'uf.sender_user_id', 'sender.name as sender_name', 'sender.image_url', 'flowers.name as flower_name', 'uf.message', 'uf.created_at')
+    .where({'uf.reciever_user_id': req.params.id})
+    .andWhere('uf.id', '>', begin_id)
+    .limit(limit)
+    .then(function(rows) {
+      return res.status(200).json({
+        send: rows.map(function(elem) {
+          return {
+            send_id: elem.id,
+            sender: {
+              id: elem.sender_user_id,
+              name: elem.sender_name,
+              image_url: elem.image_url 
+            },
+            flower_name: elem.flower_name,
+            message: elem.message,
+            sent_at: elem.created_at
+          }
+        })
+      });
     });
-  return next();
 });
 
 router.post('/:id/flowers', function (req, res, next) {
 
-  //Need to test following code
-  // knex('flowers').where('name', req.params.flower_name)
-  //   .then(function(row){
-  //     if(!row[0]) {
-  //       res.status(404).send({
-  //         "message": "Flower Not Found"
-  //       });       
-  //     }
-  //     knex('user_flowers').insert({sender_user_id: req.body.sender_id, reciever_user_id: req.body.reciever_id, flower_id: row.id, message: req.body.message})
-  //       .then(function() {
-  //         return res.status(201);
-  //       }).catch(function(err) {
-  //         return res.status(400);
-  //       });
-  //   });
-  res.status(201);
-  return next();
+  knex('flowers').where('name', req.body.flower_name)
+    .then(function(row){
+      if(!row[0]) {
+        res.status(404).send({
+          "message": "Flower Not Found"
+        });       
+      }
+      knex('user_flowers').insert({sender_user_id: req.body.sender_id, reciever_user_id: req.body.reciever_id, flower_id: row[0].id, message: req.body.message})
+        .then(function(row) {
+          return res.status(201).send({
+            "id": row
+          });
+        }).catch(function(err) {
+          return res.status(400).json(err);
+        });
+    });
 });
 
 
